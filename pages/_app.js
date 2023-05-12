2e9a013764b4d55401054f2af2f309bd6a182672
muni <booscarface@gmail.com>
2023-05-12 16:54:49 +0200

    title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+     <Component {...pageProps} />
+     <footer>
+      <p>© 2023 sashacorp </p>
+     </footer>
+  </>)
 }
 
 export default MyApp
