75e60a1a03a6f21dbcbbf067e1d6cd081e2b24be
muni <booscarface@gmail.com>
2023-05-17 12:22:52 +0200

    added media query

--- components/banner.js
+++ components/banner.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+
 import styles from './banner.module.css'
 
 export default function Banner (props) {
@@ -6,14 +6,13 @@ export default function Banner (props) {
     <div className={styles.container}>
 
     {/* Title */}
-      <div className={styles.titleContainer}>
+      
       <h1 className={styles.title}>
           SILVESTRO'S
-          <br></br>
-          <span  className={styles.titleSpan}>
-            TRAVEL GUIDE
-          </span>
-        </h1>
+      </h1>
+      <h1 className={styles.titleSpan}>
+          TRAVEL GUIDE
+      </h1>
        <p className={styles.subTitle}>Discover the local coffee shops</p>
        <div className={styles.buttonWrapper}>
        <button 
@@ -23,14 +22,9 @@ export default function Banner (props) {
           {props.buttonText}
           </button>
        </div>
-      </div>
+     
 
-      {/* Hero  */}
-      <div className={styles.heroContainer}>
-      <div className={styles.heroImage}>
-        <Image src="/static/hero.png" width={500} height={500} alt="hero" priority/>
-        </div>
-      </div>
+      
        
     </div>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import Head from 'next/head'
-
+import Image from 'next/image'
 import Banner from '../components/banner'
 import styles from '../styles/Home.module.css'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { useTheme } from '@mui/material/styles'
+
 
 export default function Home() {
+  const theme = useTheme()
+  const matchesMD = useMediaQuery(theme.breakpoints.up('md'))
+
+ 
   const handleBannerOnClick = () => {
     console.log('hi, banner button')
   }
@@ -17,12 +24,22 @@ export default function Home() {
 
       <main className={styles.main}>
         
+        <section className={styles.welcomeSectionContainer}>
+        <div className={styles.bannerContainer}>
         <Banner 
             buttonText="View stores nearby" 
             handleOnClick={handleBannerOnClick}
         />
+        </div>
         
-        
+        {/* Hero  */}
+       { matchesMD &&
+        <div className={styles.heroContainer}>
+      <div className={styles.heroImage}>
+        <Image src="/static/hero.png" width={500} height={500} alt="hero" priority/>
+        </div>
+      </div>}
+        </section>
       </main>
     </div>
   )
